3abdb8a7e0f24518cd4c8242fe46e3a61cca906a
Christopher Luna <mycql04@gmail.com>
2017-02-25 04:51:48 +0000

    +Added resume content and styling

--- scripts/render.js
+++ scripts/render.js
@@ -2,37 +2,47 @@ const fs = require('fs');
 const pug = require('pug');
 const yaml = require('js-yaml');
 const sass = require('node-sass');
+const postcss = require('postcss');
+const autoprefixer = require('autoprefixer');
 const templatesPath = './src/templates/';
 const dataPath = './src/data/';
 const stylesPath = './src/styles/styles.scss';
 
 function render(targetPath) {
-  console.info('Compiling styles...');
+  console.info('Compiling Sass files...');
   sass.render({
     file: stylesPath,
     outputStyle: 'compressed'
-  }, (error, result) => {
-    if (error) {
-      console.error(error.status);
-      console.error(error.column);
-      console.error(error.message);
-      console.error(error.line);
+  }, (sassError, sassResult) => {
+    if (sassError) {
+      console.error(sassError.status);
+      console.error(sassError.column);
+      console.error(sassError.message);
+      console.error(sassError.line);
     } else {
-      console.info('Styles done.');
-      console.info('Rendering templates ...');
-      try {
-        const data = yaml.safeLoad(fs.readFileSync(`${dataPath}resume.yml`, 'utf8'));
-        data.styles = result.css.toString();
-        const compileFn = pug.compileFile(`${templatesPath}main/template.pug`, { pretty: true });
-        const html = compileFn(data);
-        console.info('Templates Done.');
-        console.info('Generating index file ...');
-        fs.writeFileSync(targetPath, html, { encoding: 'UTF-8' });
-        console.info('Index File Done.');
-      } catch (e) {
-        console.error('Error while rendering template.');
-        console.error(e);
-      }
+      console.info('Sass done.');
+      const css = sassResult.css.toString();
+      console.info('Running PostCSS...');
+      postcss([autoprefixer]).process(css).then((postcssResult) => {
+        console.info('PostCSS done.');
+        console.info('Rendering templates ...');
+        try {
+          const data = yaml.safeLoad(fs.readFileSync(`${dataPath}resume.yml`, 'utf8'));
+          data.styles = postcssResult.css;
+          const compileFn = pug.compileFile(`${templatesPath}main/template.pug`, { pretty: true });
+          const html = compileFn(data);
+          console.info('Templates Done.');
+          console.info('Generating index file ...');
+          fs.writeFileSync(targetPath, html, { encoding: 'UTF-8' });
+          console.info('Index File Done.');
+        } catch (e) {
+          console.error('Error while rendering template.');
+          console.error(e);
+        }
+      }, (postcssError) => {
+        console.error('Error in PostCSS.');
+        console.error(postcssError);
+      });
     }
   });
 }
